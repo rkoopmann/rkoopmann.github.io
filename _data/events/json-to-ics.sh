df3cb96100d1d9804e72adcae0194c0b746e8bcf
rkoopmann <email@rkoopmann.com>
2023-09-04 19:56:07 -0500

    events update
    added geo

--- _data/events/json-to-ics.sh
+++ _data/events/json-to-ics.sh
@@ -9,11 +9,11 @@ PRODID:A HACK
 CALSCALE:GREGORIAN
 X-WR-TIMEZONE:America/Chicago
 X-WR-CALNAME:Setlist.fm
-X-WR-CALDESC:Sourced from selist.fm exportInc._products
+X-WR-CALDESC:Sourced from selist.fm export
 X-WR-RELCALID:8765309E-E4AC-4C2C-B9F1-FF78AB91DA97
 X-APPLE-CALENDAR-COLOR:#000080" > "${output_file}"
 
-cat list.json| jq -r '.[][] | select(.) | . as $i |
+cat list.json | jq -r '.[][] | select(.) | . as $i |
 "BEGIN:VEVENT
 UID:\($i.Id)
 SUMMARY:\($i.Artist)
@@ -21,6 +21,7 @@ DTSTART;VALUE=DATE:\($i.Date | gsub("-"; ""))
 DTEND;VALUE=DATE:\($i.Date | gsub("-"; ""))
 URL:\($i.Link)
 LOCATION:\($i.City), \($i.State)
+GEO:\($i.Lat);\($i.Long)
 DESCRIPTION:#\($i.Artist | gsub(" "; "-")) #\($i.Venue | gsub(" "; "-"))
 END:VEVENT"' | sed "s/$/$CRLF/" >> "${output_file}"
 
