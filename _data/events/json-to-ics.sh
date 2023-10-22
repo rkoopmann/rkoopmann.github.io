0d760768c4fd23fd65fde1752a0d353156f5227c
rkoopmann <email@rkoopmann.com>
2023-10-21 22:37:23 -0500

    fixin up the ics

--- _data/events/json-to-ics.sh
+++ _data/events/json-to-ics.sh
@@ -9,7 +9,7 @@ PRODID:A HACK
 CALSCALE:GREGORIAN
 X-WR-TIMEZONE:America/Chicago
 X-WR-CALNAME:Setlist.fm
-X-WR-CALDESC:Sourced from selist.fm export
+X-WR-CALDESC:Sourced from selist.fm export files
 X-WR-RELCALID:8765309E-E4AC-4C2C-B9F1-FF78AB91DA97
 X-APPLE-CALENDAR-COLOR:#000080" > "${output_file}"
 
@@ -17,6 +17,7 @@ cat {1,2}*.json | jq -r '. |
 "BEGIN:VEVENT
 UID:\(.id)
 SUMMARY:\(.artist.name)
+DTSTAMP;TZID=America/Chicago:\(.eventDate | split("-") | .[2] + .[1] + .[0])T200000
 DTSTART;VALUE=DATE:\(.eventDate | split("-") | .[2] + .[1] + .[0])
 DTEND;VALUE=DATE:\(.eventDate | split("-") | .[2] + .[1] + .[0])
 URL:\(.url)
